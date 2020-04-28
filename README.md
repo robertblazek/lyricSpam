a0b1f779b6ceab11207f6d0b401112ca2defc64c
Robert Blažek <me@rblazek.cz>
2020-04-28 15:02:34 +0200

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # lyricSpam
- Basic Python lyrics spammer
+ #### Basic Python lyrics spammer.
+
+Just get the lyrics and paste it in the `lyrics.txt` file. Each word is sent individually. You get 10 seconds to get a text field in focus. After every word, the `[ENTER]` key is pressed to send the message.
+
+## Dependencies
+You need __Python__ and the `pyautogui` library. [\(Install it here\)](https://pyautogui.readthedocs.io/en/latest/install.html#)
+
+I don't take any responsibility for what is being done with this script.
---
+++ lyricspam.py
@@ -0,0 +1,37 @@
+import pyautogui
+import time
+
+print("This is a lyrics spammer script. - Why? Why not. \n Make sure you have a text field in focus. \n Be careful not to lose friends. \n Hit ^C  anytime to stop. \n")
+input('Press ENTER to continue...')
+print("Starting in 10...")
+time.sleep(1)
+print("Starting in 9...")
+time.sleep(1)
+print("Starting in 8...")
+time.sleep(1)
+print("Starting in 7...")
+time.sleep(1)
+print("Starting in 6...")
+time.sleep(1)
+print("Starting in 5...")
+time.sleep(1)
+print("Starting in 4...")
+time.sleep(1)
+print("Starting in 3...")
+time.sleep(1)
+print("Starting in 2...")
+time.sleep(1)
+print("Starting in 1...")
+time.sleep(1)
+print("Starting now... \n")
+
+#sourceFile = str(open("lyrics.txt","r"))
+with open('lyrics.txt', 'r') as file:
+    sourceFile = file.read().replace('\n', '')
+words = sourceFile.split()
+#print(words)
+#pyautogui.press('playpause')
+for word in words:
+    pyautogui.typewrite(word)
+    pyautogui.press('enter')
+    print('Typing '+ word)
